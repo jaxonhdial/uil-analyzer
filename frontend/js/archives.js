af2bc1ca95c4042777c5f752c99ffb385558b3c5
jaxonhdial <jaxonhdial@gmail.com>
2025-12-16 15:55:02 -0600

    About page is finished and v0.1 is completed!

--- frontend/js/archives.js
+++ frontend/js/archives.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const districtRadio = document.getElementById("levelDistrict");
     const regionRadio = document.getElementById("levelRegion");
